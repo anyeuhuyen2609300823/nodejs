78fe2091b3f2d1724e3dbeee768e39f0c0d52f5a
your name <abc@gmail.com>
2023-04-01 13:23:45 +0700

    lesson5

---
+++ poly-nodejs-WE17317/lesson5/src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+import userRouter from "./router/user";
+
+const app = express();
+const port = 8080;
+
+// Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Router
+app.use("/auth", userRouter);
+
+// MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/we17317")
+  .then(() => console.log("Connected to DB"));
+
+app.listen(port, () => {
+  console.log("Server is running on " + port);
+});
